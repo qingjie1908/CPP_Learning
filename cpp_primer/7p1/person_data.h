55a48decbd7293bb615597a581e9688391a687ae
Qingjie <qingjie1908@outlook.com>
2024-03-11 16:57:07 +0800

    add read_person and print_person func

--- cpp_primer/7p1/person_data.h
+++ cpp_primer/7p1/person_data.h
@@ -13,4 +13,12 @@ struct Person{
     std::string get_name() const {return name;} // not need to change name, so use const to indicate *this.get_name() is read only 
     std::string get_addr() const {return address;} // get func only read object member
 };
+std::istream &read_person(std::istream &is, Person &pers)
+{
+    is >> pers.name >> pers.address;
+}
+std::ostream &print_person(std::ostream &os, const Person &pers)
+{
+    os << pers.get_name() << pers.get_addr();
+}
 #endif
