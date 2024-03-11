ac38a055b344226e20d98e6d218201003b9d1d41
Qingjie <qingjie1908@outlook.com>
2024-03-11 15:43:45 +0800

    add member function declaration and defination of isbn(), avg_price()  and combine()

--- cpp_primer/2p6/Sales_data.h
+++ cpp_primer/2p6/Sales_data.h
@@ -20,4 +20,17 @@ struct Sales_data {
 Sales_data add(const Sales_data&, const Sales_data&);
 std::ostream &print(std::ostream&, const Sales_data&);
 std::istream &read(std::istream&, Sales_data&);
+Sales_data& Sales_data::combine(const Sales_data &rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this; // if we declare object Sales_data total; then call total.combine(trans), then *this return 'total' object
+}
+double Sales_data::avg_price() const
+{
+    if (units_sold)
+        return revenue/units_sold;
+    else
+        return 0;
+}
 #endif
