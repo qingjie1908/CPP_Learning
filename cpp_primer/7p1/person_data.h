31c01c237985a11e3368bf22b14892eb2ac8bef5
Qingjie <qingjie1908@outlook.com>
2024-03-12 18:52:16 +0800

    ex 7.15 add default and other constructors for person_data.h

--- cpp_primer/7p1/person_data.h
+++ cpp_primer/7p1/person_data.h
@@ -12,6 +12,8 @@ struct Person{
     std::vector<std::string> vec1 = {name, address};
     std::string get_name() const {return name;} // not need to change name, so use const to indicate *this.get_name() is read only 
     std::string get_addr() const {return address;} // get func only read object member
+    Person():name("Qingjie"),address("Library"){} // own version defalut constructor
+    // if use Person() = default; name and address will be empty string 
 };
 std::istream &read_person(std::istream &is, Person &pers)
 {
