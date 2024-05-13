2c848ce8f718ac6590533807991e258381465794
Qingjie <qingjie1908@outlook.com>
2024-05-13 18:32:04 +0800

    partition with predicate

---
+++ cpp_primer/10p3/10p13.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+bool predicate_check_string(std::string& str)
+{
+    return str.size() >= 5;
+}
+
+int main()
+{
+    std::vector<std::string> vec_s = {"dsa", "sa", "aaefw", "wqrget", "dfsa", "bv"};
+
+    auto iter_part = std::partition(vec_s.begin(), vec_s.end(), predicate_check_string);
+    //partition that takes a pred- icate and partitions the container 
+    //so that values for which the predicate is true appear in the first part 
+    //and those for which the predicate is false appear in the second part
+
+    //returns an iterator just past the last element for which the predicate returned true.
+
+    //print the first part of partitioned vec_s
+
+    auto iter = vec_s.begin();
+    
+    while(iter != iter_part){ //iter_part point to first string element which size is less than 5
+        std::cout << *iter << " ";
+        ++iter;
+    }
+    //wqrget aaefw
+
+    return 0;
+}
