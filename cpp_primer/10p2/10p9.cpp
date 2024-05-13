6b5494b703a510722a2ce7cecc915a98a5ff5d0d
Qingjie <qingjie1908@outlook.com>
2024-05-13 15:32:03 +0800

    ex 10.9 sort and unique func

---
+++ cpp_primer/10p2/10p9.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+void elimDups(std::vector<std::string> &vec)
+{
+    //first, sort the string so that dupicates appera adjacent in the vector
+    std::sort(vec.begin(), vec.end());
+
+    //print sorted vec
+    std::cout << "sorted string vec are: " << std::endl;
+    for(auto item : vec){
+        std::cout << item << " ";
+    }
+    std::cout << std::endl;
+
+    //now use unique to so that each word appears once in the
+    // front portion of the range and returns an iterator one past the unique range
+    auto end_unique = std::unique(vec.begin(), vec.end());
+
+    //vec size is not changed by uniq(), print vec now
+    std::cout << "sorted string vec with only unique value in the front portion has size: " << vec.size() << std::endl;
+    std::cout << "sorted string vec with only unique value in the front portion are: " << std::endl;
+    for(auto item : vec){
+        std::cout << item << " ";
+    }
+    std::cout << std::endl;
+
+    //now earse the duplicates after the end_unique iter (including this)
+    vec.erase(end_unique, vec.end()); //if end_unique = vec.end(), then the range is empty since iter range are left inclusive
+    //[ vec.end(),  vec.end() ) means empty range, takes no effect
+
+    //now print the size changed vec
+    std::cout << "sorted string vec with duplicates erased are: " << std::endl;
+    for(auto item : vec){
+        std::cout << item << " ";
+    }
+    std::cout << std::endl;
+
+}
+
+int main()
+{
+    std::vector<std::string> vec_s = {"the", "quick", "red", "fox", "jumps", "over", "the", "slow", "red", "turtle"};
+
+    elimDups(vec_s);
+
+    return 0;
+}
