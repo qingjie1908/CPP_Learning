c7d8bd4a0a6205991a39f0bd5d1d575ee769e67d
Qingjie <qingjie1908@outlook.com>
2024-05-25 18:24:31 +0800

    ex 12.27 use two class shared ptr map set to write the textquery program

---
+++ cpp_primer/12p2/12p27_QueryResult.h
@@ -0,0 +1,58 @@
+#ifndef QUERYRESULT
+#define QUERYRESULT
+
+#include <iostream>
+#include <string>
+#include <memory>
+#include <vector>
+
+class QueryResult{
+friend std::ostream& print(std::ostream& os, QueryResult& result_obj);
+public:
+    //constructor
+    //p_tq is the shared pointer in TextQuery class
+    QueryResult(std::shared_ptr<std::vector<std::pair<int, std::string>>> p_tq, std::shared_ptr<int> p_oc): p_result(p_tq), p_occur(p_oc) {}
+
+    QueryResult(){
+        //default constructor point to empty vec
+        p_result = std::make_shared<std::vector<std::pair<int, std::string>>>();
+
+        //default constructor occur hold 0
+        p_occur = std::make_shared<int>(0);
+
+    } 
+    
+    
+private:
+    //shared_ptr to hold a result vec, element is a pair <line number, line_string>
+    std::shared_ptr<std::vector<std::pair<int, std::string>>> p_result;
+
+    //p_occur hold count of how many times requrest word appear in the file
+    std::shared_ptr<int> p_occur;
+
+
+
+    //after a TextQuery obj call obj.query(word), p_result may have results or a nullptr
+
+};
+
+//non-member func print
+std::ostream& print(std::ostream& os, QueryResult& result_obj){
+    //first check if there is any result
+    if(!result_obj.p_result->empty()){
+        //first print the word occurence in the whole file
+        os << "element occurs " << *result_obj.p_occur << " times: " << std::endl;
+
+        //then print each line that contains this word
+        auto curr = result_obj.p_result->begin();
+        while(curr != result_obj.p_result->end()){
+            os << "\t" << "(line " << curr->first << ") " << curr->second << std::endl;
+            ++curr;
+        }
+    }
+    return os;
+}
+
+
+
+#endif
---
+++ cpp_primer/12p2/12p27_TextQuery.h
@@ -0,0 +1,138 @@
+#ifndef TEXTQUERY
+#define TEXTQUERY
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <memory>
+#include <string>
+
+#include "/Users/qingjie/github/CPP_Learning/cpp_primer/12p2/12p27_QueryResult.h"
+
+
+
+class TextQuery{
+friend void generate_vec_file(std::ifstream& in_f, std::vector<std::string>& vec_f);
+friend void generate_map(std::vector<std::string>& vec_r, std::set<int>& line_record_r, std::map<std::string, std::set<int>>& word_line_map_r, std::map<std::string, int>& word_count_map_r);
+//friend class QueryResult;
+public:
+    //constructor declaration, using ifstream to store the input file
+    TextQuery(std::ifstream &input_f);
+
+    //member func query(), to find the line for given word
+    //return a QueryResult obj
+    QueryResult query(std::string find_word);
+
+private:
+    std::ifstream& input_file; //note when we pass stream as constructor element, it's a reference, cannot copy
+
+    //first, need to store the file into vector line by line, this is done in constructor
+    std::vector<std::string> vec_file;
+
+    std::set<int> line_record; //hold the line numbers on which each word appears
+    std::map<std::string, std::set<int>> word_line_map; //map, word and its corresponding set holding line number on which word appear
+    std::map<std::string, int> word_count_map; //map, word and its occurrence in the file
+
+};
+
+//non-member function
+void generate_vec_file(std::ifstream& in_f, std::vector<std::string>& vec_f){
+    std::string read_line;
+    while(getline(in_f, read_line)){
+        vec_f.push_back(read_line);
+    }
+}
+
+//non-member function
+void generate_map(std::vector<std::string>& vec_r, std::set<int>& line_record_r, std::map<std::string, std::set<int>>& word_line_map_r, std::map<std::string, int>& word_count_map_r){
+    auto iter = vec_r.begin();
+    int line_number = 1; // let line number start from 1
+    while(iter != vec_r.end()){
+        std::istringstream i_str(*iter); //store a line in i_str
+        
+        std::string word;
+        while(i_str >> word){
+
+            //first process this word
+            //because in a file it can be "word", "word, word", word!, word? ...
+            std::string punc = "?'!.\",:;";
+            size_t pos = 0;
+            while((pos = word.find_first_of(punc)) != std::string::npos){
+                word.erase(pos, 1); //remove char staring from word[pos] with length 1, so remove one char, return reference to this word
+            }
+
+
+            if(!word_line_map_r.count(word)){ //this word not in the map yet
+                //create a pair
+                std::pair<std::string, std::set<int>> p = {word, {line_number}};
+                word_line_map_r.insert(p);
+
+                //also create its occrence in word_count map
+                word_count_map_r[word] = 1;
+            } else { //this word already in map, add its line number in set
+                (word_line_map_r[word]).insert(line_number);
+                //word_line_map_r[word] is its corresponding set
+                //not it's not a multiset, so if the same word happens twice in the same line, line_number only record once
+
+                //also update the occurrence of this word
+                ++word_count_map_r[word];
+
+            }
+        }
+        //now the current line process finished
+        //move to next line
+        ++iter;
+        //also remember update its corresponding line_number
+        ++line_number;
+    }
+}
+
+//constructor define
+//note generate_vec_file() and generate_map() must be definded before it can be used
+//or use forward decalration first then defind after constructor
+TextQuery::TextQuery(std::ifstream &input_f): input_file(input_f) {
+        generate_vec_file(input_file, vec_file);
+        generate_map(vec_file, line_record, word_line_map, word_count_map);
+
+}
+
+
+//member func query
+QueryResult TextQuery::query(std::string find_word){
+
+    //creat a shared_ptr so that memory can be shared to QueryResult class obj
+    //note result should contain two part, first line number, seconde line result
+    //so p_hold is a vec, each element is a pair of <line_number, string_line>
+    //p_hold now point to empty vec
+    std::shared_ptr<std::vector<std::pair<int, std::string>>> p_hold = std::make_shared<std::vector<std::pair<int, std::string>>> ();
+
+    //also create another share ptr hold this word occurence in the whole file
+    std::shared_ptr<int> occurrence = std::make_shared<int>(0); //initialized to 0
+    
+
+    //use p_hold and occurrence to construct a QueryResult class obj
+    QueryResult obj(p_hold, occurrence);
+
+    if(word_line_map.count(find_word)){ //there is a word in this file
+
+        //word_line_map[find_word] is its corresponding set holding line number
+        //find each line in vec_file using this line number
+
+        for(auto line_number : word_line_map[find_word]){
+            p_hold->push_back({line_number, vec_file[line_number-1]}); //push back a pair {line_number, line_string}
+        }
+
+        //find this find_word occurence in word_count map
+        *occurrence = word_count_map[find_word];
+
+    }
+    //else then not find, p_hold and p_result are still empty vec, occurrence is 0
+
+    return obj; 
+
+} // after call query(), p_hold, occurrence destroyed, but p_result and p_occur in returned obj that call this query() func still exist
+
+#endif
