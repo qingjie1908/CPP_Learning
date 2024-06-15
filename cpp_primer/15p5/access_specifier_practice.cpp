dfb3cef47ffd26b1dd7ce6e1f3d4e93b8a9aa489
Qingjie <qingjie1908@outlook.com>
2024-06-15 22:24:26 +0800

    access specifier practice, friend class B of A can access  class A  all member, no matter from obeject of type A, or the obeject is just embedded in an derived_class object

--- cpp_primer/15p5/access_specifier_practice.cpp
+++ cpp_primer/15p5/access_specifier_practice.cpp
@@ -15,17 +15,21 @@
 //printe_total(obj_derived) and obj_derived.isbn() is ok or not depend on the access specifier in derivation list
 //so public in derivation list is key to dynamic binding, if its private, then you cannot bind a Derived_obj to a Base type reference, cause the Base member even public is not seen by this derived obj
 
-//so user "class Bulk_quote" of "class Quote" can access which depend on the access specifier inside base class "Quote"
-//user obj_derived of "class Bulk_quote" can access which depend on the access specifier inside "class Bulk_Quote",
+//so user "class Bulk_quote" of "class Quote" can access which in base depend on the access specifier inside base class "Quote"
+//user obj_derived (object of Bulk_quote or derived-class of Bulk_quote) of "class Bulk_quote" can access which in base depend on the access specifier inside "class Bulk_Quote",
 //obj_derived whether can access base class public member and be treated as base class object, depends on the access specifier in the derivation list
+//if derivation list is protected, then public member of Base would be protected member in Bulk_quote
+//if class next_derived_class : public Derived; then next_derived_class can access these protect member in Bulk_quote,
+//object of type next_derived_class
 
 //When the derivation is public, the public members of the base class become part of the interface of the derived class as well. 
 //In addition, we can bind an object of a publicly derived type to a pointer or reference to the base type. key for dynamic binding
 
-
+#include <iostream>
 
 
 class Base{
+friend class Pal;
 public:
     int base_pub;
 
@@ -47,6 +51,32 @@ private:
 protected:
     int deri_pro;
 };
+
+//here user of "class Derived" is "class next_Derived", it is not an object, but a class
+//so it can access "class Derived" public and protected member
+//if class Derived : public Base, then it can be see public member in Base is still public in Derived
+                            //protected member in Base is still protected in Derived
+                            //so user of "class Derived" can access Base public, if user is a class, then it can also access Base protected
+
+//if class Derived : protected Base, then it can be see public member in Base becomes proteced in Derived,
+                            //protected member in Base is still protected in Derived
+                            //so user of "class Derived" if its an object, then can not access Base public, also cannot access Base protected
+                            // if user is a class, like "class next_Derived", then it can access Base public, and can also access Base protected
+
+//if class Derived : private Base, then all member in Base become private in class Derived, 
+                    //"class next_derived" cannot access any member in base
+class next_Derived : public Derived{
+    int a = deri_pub; //ok
+    int b = deri_pro; //ok
+    int c = deri_pri; //error
+    int d = base_pub; //ok if class Derived : public Base, or class Derived : protected Base
+    //error if class Derived : private Base
+    int e = base_pro; //ok if class Derived : public Base, or class Derived : protected Base
+    //error if class Derived : private Base
+};
+
+next_Derived objj;
+int aaa = objj.base_pub;
 
 //class Derived itself has access to itself all member, and Base class base_pro, base_pub, but generally should not assign them, just read them
 //friend func1 of class Derived has same access level as Derived, so Derived all member, and Base public and protected
@@ -116,6 +146,36 @@ void func4(Derived& dobj){ //not friend, also note no base to derive conversion
     dobj.deri_pub;
     dobj.base_pub; //if derivation list public, ok, if private, error
 }
+
+class Pal {
+    Derived obj;
+    int a = obj.deri_pub;
+    int b = obj.deri_pri; //error, Pal not friend of Derived
+
+    //c d e is suprise
+    //if in main func or other scope, c is allowed only class Derived : public Base, d, e not allowd
+    //but here it access base_pri and base_pro inside class Pal
+    //and class Pal is a friend class of class Base
+    //so inside class Pal, its data member and member func can access Base objects all member
+    //no matter this base object is a base type, or is a part that embedded in an object of derived type
+    int c = obj.base_pub; 
+    int d = obj.base_pri;
+    int e = obj.base_pro;
+
+    Base bobj;
+    int aa = bobj.base_pub;
+    int bb = bobj.base_pri;
+    int cc = bobj.base_pro;
+
+    int func(Derived obj) {return obj.base_pub;}
+
+    
+     //this is ok even if class Derived : private base
+    //because class Pal is friend class of Base
+    //inside class pal, its data member or member func can access the Base objects
+    //or the Base object is embedded in an derived_class object
+};
+
 
 int main()
 {
